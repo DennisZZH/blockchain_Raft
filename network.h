5f1bf7272b0301ddd6092bff69369ca521c8b77b
boning <boning@ucsb.edu>
2020-11-27 20:47:29 -0800

    Added raft related structs. Setup the statemachine framework.

--- network.h
+++ network.h
@@ -2,17 +2,32 @@
 #include <iostream>
 #include <stdint.h>
 #include <thread>
+#include "raft.h"
 
 #define CLIENT_IP           "127.0.0.1"
 #define SERVER_BASE_PORT    8000
 #define CLIENT_BASE_PORT    8010
 #define MAX_CLIENT_NUM      3
 
+
 struct client_info_t {
     int socket;
     bool valid;
     std::thread task;
 };
+
+typedef enum {
+    NONE,
+    REQ_VOTE_RPC,               // request vote RPC
+    REQ_VOTE_RPL,               // request vote reply
+    APP_ENTR_RPC,               // append entry RPC
+    APP_ENTR_RPL                // append entry reply
+} msg_type_t;
+
+typedef struct {
+    msg_type_t type;            // type determines which struct we should cast the message payload to.
+    void* payload;              // cast this one to one of the message struct.
+} msg_t;
 
 class Network {
 private:    
